683cf880c3ef7b5df51f2ad3eecf9857b07f26b8
Darya Platonova <searching.for.a.sign@gmail.com>
2023-09-07 12:54:25 +0300

    add custom checkbox

--- src/components/to-do-list-item/to-do-list-item.tsx
+++ src/components/to-do-list-item/to-do-list-item.tsx
@@ -21,11 +21,26 @@ export const ToDoListItem = (props: ToDoListItemProps) => {
       <input
         id={String(id)}
         type="checkbox"
-        className="checked:bg-lime-300"
+        className="checked:bg-lime-300 peer relative appearance-none shrink-0 w-4 h-4 border-2 border-lime-400 rounded-sm mt-1 bg-white
+        focus:outline-none focus:ring-offset-0 focus:ring-1 focus:ring-lime-100
+         checked:border-0
+        disabled:border-stone-100 disabled:bg-stone-100"
         onChange={handleChange}
         checked={done}
         name={label}
       />
+      <svg
+        className="absolute w-4 h-4 pointer-events-none hidden peer-checked:block stroke-white mt-1 outline-none"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <polyline points="20 6 9 17 4 12"></polyline>
+      </svg>
       <label htmlFor={String(id)} className={labelclassname}>
         {label}
       </label>
